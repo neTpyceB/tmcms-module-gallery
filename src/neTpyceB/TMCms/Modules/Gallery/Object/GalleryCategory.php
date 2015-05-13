783b826f7056955a2168848577c1fa5b3b84d725
Vadims Petrusevs <pvman@inbox.ru>
2015-05-13 20:25:46 +0300

    Code updates, removed odd methods

--- src/neTpyceB/TMCms/Modules/Gallery/Object/GalleryCategory.php
+++ src/neTpyceB/TMCms/Modules/Gallery/Object/GalleryCategory.php
@@ -6,13 +6,15 @@ use neTpyceB\TMCms\Modules\CommonObject;
  * Class GalleryCategory
  * @package neTpyceB\TMCms\Modules\Gallery\Object
  *
+ * @method string getTitle()
+ * @method bool getActive()
+ * @method int getOrder()
+ *
+ * @method setTitle(array)
+ * @method setActive(bool)
  * @method setOrder(int)
  */
 class GalleryCategory extends CommonObject {
     protected $db_table = 'm_gallery_categories';
     protected $multi_lng_fields = ['title'];
-
-    protected $title = '';
-    protected $active = '';
-    protected $order = 0;
 }
